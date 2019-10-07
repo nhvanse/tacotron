3033457bce63f8f3a306a9f59b93ae8d1d1dd51d
van26101998 <35824966+van26101998@users.noreply.github.com>
2019-10-07 19:00:17 +0700

    Update train.py

--- train.py
+++ train.py
@@ -256,6 +256,7 @@ def train(output_directory, log_directory, checkpoint_path, warm_start, n_gpus,
                     os.rename(checkpoint_path, '/kaggle/working/checkpoint_{}'.format(iteration))
                     from IPython.display import FileLink
                     os.chdir('/kaggle/working')
+                    os.system('rm checkpoint_{}'.format(iteration - hparams.iters_per_checkpoint))
                     print(FileLink('checkpoint_{}'.format(iteration)))
                     os.chdir('/kaggle/working/tacotron/')
 
